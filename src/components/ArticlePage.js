625c3fe61d37bb6227a733691b1e53ce6faac71d
allanstar-byte <allanmutisya263@gmail.com>
2022-07-25 10:52:24 +0300

    useParams

--- src/components/ArticlePage.js
+++ src/components/ArticlePage.js
@@ -1,44 +1,35 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useParams } from "react-router-dom";
+import useDocumentTitle from "../hooks/useDocumentTitle";
+import useQuery from "../hooks/useQuery";
 import { makeEmojiList } from "../utils";
 
 function ArticlePage() {
-  // fetch data for a post
-  const { id } = useParams();
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [post, setPost] = useState(null);
+    // fetch data for a post
+    const { id } = useParams();
+    const { data: post, isLoaded } = useQuery(
+        `http://localhost:4000/posts/${id}`
+    );
 
-  const url = `http://localhost:4000/posts/${id}`;
-  useEffect(() => {
-    setIsLoaded(false);
-    fetch(url)
-      .then((r) => r.json())
-      .then((post) => {
-        setPost(post);
-        setIsLoaded(true);
-      });
-  }, [url]);
+    // set the document title
+    const pageTitle = post ? `Underreacted | ${post.title}` : "Underreacted";
+    useDocumentTitle(pageTitle);
 
-  // set the document title
-  const pageTitle = post ? `Underreacted | ${post.title}` : "Underreacted";
-  useEffect(() => {
-    document.title = pageTitle;
-  }, [pageTitle]);
+    if (!isLoaded) return <h3 > Loading... < /h3>;
 
-  if (!isLoaded) return <h3>Loading...</h3>;
+    const { minutes, title, date, preview } = post;
+    const emojis = makeEmojiList(minutes);
 
-  const { minutes, title, date, preview } = post;
-  const emojis = makeEmojiList(minutes);
-
-  return (
-    <article>
-      <h3>{title}</h3>
-      <small>
-        {date} • {emojis} {minutes} min read
-      </small>
-      <p>{preview}</p>
-    </article>
-  );
+    return ( <
+        article >
+        <
+        h3 > { title } < /h3> <
+        small > { date }• { emojis } { minutes }
+        min read <
+        /small> <
+        p > { preview } < /p> <
+        /article>
+    );
 }
 
 export default ArticlePage;
